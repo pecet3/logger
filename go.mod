118933fa549392c90e23643ffc335c7c7ad93124
Jakub <pecet3107@gmail.com>
2025-01-02 18:27:50 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pecet3/logger
+
+go 1.23.4
---
+++ logger.go
@@ -0,0 +1,123 @@
+package logger
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+)
+
+func Error(args ...interface{}) {
+	pc, _, line, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	fName := fn.Name()
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s (%s:%s)`,
+		formatTextExt(bold, red, " ERROR"),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(brightBlue, fName),
+		formatText(bold, strconv.Itoa(line)),
+	)
+	fmt.Println(content)
+	if len(args) > 0 {
+		fmt.Println("↳", formatText(bgRed, formatTextExt(bold, brightYellow, msg)))
+	}
+
+}
+
+func Info(args ...interface{}) {
+
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s %s`,
+		formatTextExt(bold, brightGreen, " INFO "),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(bold, msg),
+	)
+	fmt.Println(content)
+
+}
+
+func InfoC(args ...interface{}) {
+
+	pc, _, line, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	fName := fn.Name()
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s (%s:%s)`,
+		formatTextExt(bold, brightGreen, " INFO "),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(brightBlue, fName),
+		formatText(bold, strconv.Itoa(line)),
+	)
+	fmt.Println(content)
+	if len(args) > 0 {
+		fmt.Println("↳", formatTextExt(bold, brightYellow, msg))
+	}
+}
+func Warn(args ...interface{}) {
+
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s %s`,
+		formatTextExt(bold, orange, " WARN "),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(bold, msg),
+	)
+	fmt.Println(content)
+}
+
+func WarnC(args ...interface{}) {
+	pc, _, line, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	fName := fn.Name()
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s (%s:%s)`,
+		formatTextExt(bold, orange, " WARN "),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(brightBlue, fName),
+		formatText(bold, strconv.Itoa(line)),
+	)
+	fmt.Println(content)
+	if len(args) > 0 {
+		fmt.Println("↳", formatTextExt(bold, brightYellow, msg))
+	}
+
+}
+func Debug(args ...interface{}) {
+	pc, _, line, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	fName := fn.Name()
+	date := getCurrentDate()
+	time := getCurrentTime()
+
+	msg := fmt.Sprint(args...)
+	content := fmt.Sprintf(`[%s] %s %s (%s:%s)`,
+		formatTextExt(bold, magenta, " DBUG "),
+		formatTextExt(dim, italic, date),
+		formatText(underline, time),
+		formatText(brightBlue, fName),
+		formatText(bold, strconv.Itoa(line)),
+	)
+	fmt.Println(content)
+	if len(args) > 0 {
+		fmt.Println("↳", formatTextExt(bold, brightYellow, msg))
+	}
+}
